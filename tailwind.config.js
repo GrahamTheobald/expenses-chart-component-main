33757c5f9ef8f492e639aec851786c78d83fc57c
Graham Theobald <Graham_Theobald@live.co.uk>
2022-09-14 21:35:23 +0100

    design implemented

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        DM: ['DM Sans', 'sans-serif'],
+      },
+      colors: {
+        SoftRed: 'hsl(10, 79%, 65%)',
+        Cyan: 'hsl(186, 34%, 60%)',
+        DarkBrown: 'hsl(25, 47%, 15%)',
+        MediumBrown: 'hsl(28, 10%, 53%)',
+        Cream: 'hsl(27, 66%, 92%)',
+        VeryPaleOrange: 'hsl(33, 100%, 98%)',
+      },
+      fontSize: {
+        tiny: '0.6rem',
+      }
+    },
+  },
+  plugins: [],
+}
